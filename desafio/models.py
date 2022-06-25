7482a52d30a684ff81450cb02bf978eb490d8230
Lucas <montobybianchi@gmail.com>
2022-06-25 13:37:14 -0300

    Tercer cambio

--- desafio/models.py
+++ desafio/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Familiar(models.Model):
+class Familiar1(models.Model):
     nombre = models.CharField(max_length=30)
-    edad = models.IntegerChoices()
+    phone = models.IntegerField(verbose_name='Telefono')
     fecha_nacimiento = models.DateField(null=True)
+    
+class Familiar2(models.Model):
+    nombre = models.CharField(max_length=30)
+    phone = models.IntegerField(verbose_name='Telefono')
+    fecha_nacimiento = models.DateField(null=True)
+    
+class Familiar3(models.Model):
+    nombre = models.CharField(max_length=30)
+    phone = models.IntegerField(verbose_name='Telefono')
+    fecha_nacimiento = models.DateField(null=True)
+    
     
